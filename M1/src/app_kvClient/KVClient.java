b1c19915a02f2e7177a996d1570f13a9e0255fdc
KathyZhuang <kathyzhuang378@gmail.com>
2023-01-23 01:07:24 -0500

    first commit, added logger to client

--- M1/src/app_kvClient/KVClient.java
+++ M1/src/app_kvClient/KVClient.java
@@ -1,8 +1,17 @@
 package app_kvClient;
 
 import client.KVCommInterface;
+import logger.LogSetup;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 
 public class KVClient implements IKVClient {
+    private static Logger logger = Logger.getRootLogger(); 
+    
     @Override
     public void newConnection(String hostname, int port) throws Exception{
         // TODO Auto-generated method stub
