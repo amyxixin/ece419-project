fb98305d42314907f984859d2a64059f46201612
Jasmine Sun <jasminesun19@gmail.com>
2023-01-24 01:01:17 -0500

    implement send/receive message

---
+++ M1/src/shared/messages/MessengerModule.java
@@ -0,0 +1,95 @@
+package shared.messages;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+/**
+ * A class to provide message sending and received capacity.
+ * This class is used by both Client and ServerConnection.
+ */
+public class MessengerModule {
+
+    private Logger logger = Logger.getRootLogger();
+    private static final int BUFFER_SIZE = 1024;
+    private static final int DROP_SIZE = 128 * BUFFER_SIZE;
+
+    private Socket clientSocket;
+    private InputStream input;
+    private OutputStream output;
+
+    public MessengerModule(Socket clientSocket) throws Exception{
+        this.clientSocket = clientSocket;
+        this.output = clientSocket.getOutputStream();
+        this.input = clientSocket.getInputStream();
+    }
+
+    public void sendMessage(Message msg) throws IOException {
+        byte[] msgBytes = msg.getMsgBytes();
+        output.write(msgBytes, 0, msgBytes.length);
+        output.flush();
+    }
+
+    public Message receiveMessage() throws IOException {
+
+        int index = 0;
+        byte[] msgBytes = null, tmp = null;
+        byte[] bufferBytes = new byte[BUFFER_SIZE];
+
+        /* read first char from stream */
+        byte read = (byte) input.read();
+        boolean reading = true;
+
+        while(read != 13 && reading) {/* carriage return */
+            /* if buffer filled, copy to msg array */
+            if(index == BUFFER_SIZE) {
+                if(msgBytes == null){
+                    tmp = new byte[BUFFER_SIZE];
+                    System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+                } else {
+                    tmp = new byte[msgBytes.length + BUFFER_SIZE];
+                    System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+                    System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+                            BUFFER_SIZE);
+                }
+
+                msgBytes = tmp;
+                bufferBytes = new byte[BUFFER_SIZE];
+                index = 0;
+            }
+
+            /* only read valid characters, i.e. letters and numbers */
+            if((read > 31 && read < 127)) {
+                bufferBytes[index] = read;
+                index++;
+            }
+
+            /* stop reading is DROP_SIZE is reached */
+            if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+                reading = false;
+            }
+
+            /* read next char from stream */
+            read = (byte) input.read();
+        }
+
+        if(msgBytes == null){
+            tmp = new byte[index];
+            System.arraycopy(bufferBytes, 0, tmp, 0, index);
+        } else {
+            tmp = new byte[msgBytes.length + index];
+            System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+            System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+        }
+
+        msgBytes = tmp;
+
+        /* build final String */
+        Message msg = new Message(msgBytes);
+        return msg;
+    }
+}
